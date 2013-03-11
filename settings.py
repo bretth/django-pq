b9b177ce26f14c2428c4481384002672adae9066
Brett Haydon <brett@haydon.id.au>
2013-03-11 11:00:18 +1100

    Add logger and postgresql with autocommit true to settings

--- settings.py
+++ settings.py
@@ -1,7 +1,23 @@
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django-pq',
+        'USER': 'django-pq',
+        'PASSWORD': 'django-pq',
+        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': 5432,
+        'OPTIONS': {'autocommit': True}
+    },
+    #'auto': {
+    #    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #    'NAME': 'django-pq-auto',
+    #    'USER': 'brett',
+    #    'PASSWORD': '',
+    #    'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+    #    'PORT': 5432,
+    #    'OPTIONS': {'autocommit': True}
+    #},
+
 }
 
 INSTALLED_APPS = (
@@ -13,3 +29,27 @@ INSTALLED_APPS = (
 )
 # ROOT_URLCONF='pq.urls',
 SECRET_KEY = '1234'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+    }
+}
