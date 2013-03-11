24515aba311d32d53b2948c045049430fc01bf84
Brett Haydon <brett@haydon.id.au>
2013-03-11 16:17:14 +1100

    minor

--- settings.py
+++ settings.py
@@ -35,13 +35,13 @@ LOGGING = {
     'disable_existing_loggers': True,
     'formatters': {
         'standard': {
-            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+            'format': '[%(levelname)s] %(name)s: %(message)s'
         },
     },
     'handlers': {
         'console':{
             'level':'DEBUG',
-            'class':'logging.StreamHandler',
+            'class':"logging.StreamHandler",
             'formatter': 'standard'
         },
     },
