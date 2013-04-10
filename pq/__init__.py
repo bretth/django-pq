f57b07f13f3acce545a067872757dcaa8b3c8b6f
Brett Haydon <brett@haydon.id.au>
2013-04-10 14:37:14 +1000

    set the package namespace

--- pq/__init__.py
+++ pq/__init__.py
@@ -3,12 +3,14 @@ try:
 except:
     __version__ = ''
 
+__all__ = ['__version__', 'Queue', 'SerialQueue', 'Worker', 'Flow']
+
 from django.core.exceptions import ImproperlyConfigured
 from .queue import Queue as PQ
 from .queue import SerialQueue as SQ
 from .worker import Worker as W
+from .flow import Flow
 
 Queue = PQ.create
 SerialQueue = SQ.create
 Worker = W.create
-
